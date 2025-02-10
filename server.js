739ef8eea9e07ca3901c8f3cde523ac2309c1fd6
Dhanithya Beligolla <dbeligolla@gmail.com>
2025-02-10 14:49:48 +0530

    modify server.js and add dependancies

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const { checkDbConnection } = require("./dbConfig/dbConfig");
+
+// Check database connection
+checkDbConnection();
+
+
+//Routers
+const CustomerTestRouter = require("./routes/CustomerTest");
+const CoBorrowerRouter = require("./routes/CoBorrower");
+const CustomerRoutes = require("./routes/customerRoutes"); // New route
+
 
 const app = express();
 const PORT = process.env.PORT || 5693;
@@ -10,47 +21,18 @@ const PORT = process.env.PORT || 5693;
 app.use(cors({credentials: true, origin: true}));
 app.use(express.json());
 
-// Middleware API route
-app.get("/api/qr", async (req, res) => {
-    const { CIF_NO } = req.query;
-    if (!CIF_NO) {
-        return res.status(400).json({ error: "CIF_NO parameter is required" });
-    }
 
-    try {
-        const response = await axios.get(`http://10.18.50.145:7800/esb/DFCC_OB_NEW/v1/OB_LOAN_details?CIF_NO=${CIF_NO}`, {
-            headers: {
-                'credentials': 'OBBANK0800/AAbank@100'
-            }
-        });
-        res.json(response.data);
-    } catch (error) {
-        console.error("Error fetching data:", error.message);
-        res.status(500).json({ error: "Failed to fetch QR data" });
-    }
-});
 
-// coborrower API route
-app.get("/api/coborrower-details", async (req, res) => {
-    const { leadCustomer } = req.query;
-    if (!leadCustomer) {
-        return res.status(400).json({ error: "CIF_NO parameter is required" });
-    }
+// Use Routers
+app.use("/api/customer" ,CustomerTestRouter);
+app.use("/api/coborrower", CoBorrowerRouter);
+app.use("/api/customer", CustomerRoutes); // Add new customer route
 
-    try {
-        const response = await axios.get(`http://10.18.50.145:7800/esb/fo_coborrower/v1/getCoborrowerDetails?leadCustomer=${leadCustomer}`, {
-            headers: {
-                'credentials': 'OBBANK0800/AAbank@100'
-            }
-        });
-        res.json(response.data);
-    } catch (error) {
-        console.error("Error fetching data:", error.message);
-        res.status(500).json({ error: "Failed to fetch Coborrower data" });
-    }
-});
 
 // Start server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+    res.send("MySQL Connection Test Successful!");
+  });
